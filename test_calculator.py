65d3821fdac8c1130aac1c67f98ff0f8eb110a0f
Rodolfo Palacios <palaciosr080@gmail.com>
2019-09-18 09:28:50 -0700

    fixed whitespace issue

--- test_calculator.py
+++ test_calculator.py
@@ -5,9 +5,9 @@ class TestCalculator:
 
     def test_add(self):
 
-        assert 4 ==calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
 
     def test_subtract(self):
 
-        assert 2 ==calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
         
